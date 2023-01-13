654dacd4c105db2d2f41571ac23b46ea27865d13
Oksana-Zakhariia <okcahka.l@gmail.com>
2023-01-13 23:33:53 +0300

    Update App.js

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,7 +2,6 @@ import { AppContainer } from 'components/App/App.styled';
 import { Component } from 'react';
 import { Section } from 'components/Section/Section';
 import { ContactForm } from 'components/Form/Form';
-import { nanoid } from 'nanoid';
 
 export class App extends Component {
   state = {
